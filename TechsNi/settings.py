283d8ce59bf828a76898d6778985053abe7a8b6f
TechsNi <techsniofficial@gmail.com>
2026-08-11 09:17:12 -0700

    Switch to console email backend

--- TechsNi/settings.py
+++ TechsNi/settings.py
@@ -133,10 +133,5 @@ LOGOUT_REDIRECT_URL = 'login'
 AUTH_USER_MODEL = 'services.User'
 
 # Gmail SMTP Email Configuration via Environment Variables
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'techsniofficial@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'vskxhsvyartncleg')
-DEFAULT_FROM_EMAIL = f"TechsNi <{os.environ.get('EMAIL_HOST_USER', 'techsniofficial@gmail.com')}>"
+# Change your email backend to print to the console/logs instead of trying to connect to SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

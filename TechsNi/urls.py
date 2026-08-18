bd1a1fff69739ae19b967e19bf78132bb37ca8a1
TechsNi <techsniofficial@gmail.com>
2026-08-18 13:49:00 -0700

    Stream PDF directly from backend to avoid Cloudinary 401 error

--- TechsNi/urls.py
+++ TechsNi/urls.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import Sitemap
 from TechsNi.views import portal_gateway_view, custom_login_view
+
+# Define the sitemap class for your static/login pages
+class StaticViewSitemap(Sitemap):
+    priority = 1.0
+    changefreq = 'daily'
+
+    def items(self):
+        return ['login']  # Points to your login page's url name
+
+    def location(self, item):
+        return reverse(item)
+
+# Dictionary of sitemaps to pass to the sitemap view
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,6 +33,9 @@ urlpatterns = [
     
     path('services/', include('services.urls')),
     path('store/', include('store.urls')),
+
+    # Sitemap route
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 if settings.DEBUG:
